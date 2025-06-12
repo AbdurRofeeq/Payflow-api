11567b554276ad66c89cf7f4969051ab42f27f8c
Rofeeq <rofeeqlawal@gmail.com>
2025-06-12 17:36:35 +0300

    Updated

--- Routes/user.js
+++ Routes/user.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 const { getAllUsers, getUserById, deleteUser } = require('../Controllers/UserController');
 const auth = require('../Middleware/Auth');
-const authorizeRole = require('../middleware/authorizeRole');
+const authorizeRole = require('../middleware/AuthorizeRole');
 
 router.get('/users', auth, authorizeRole('Admin'), getAllUsers);
 router.get('/users/:id', auth, authorizeRole('Admin'), getUserById);
-router.delete('/users/:id', auth, authorizeRole('Admin'), deleteUser);
+router.delete('/users/:i0d', auth, authorizeRole('Admin'), deleteUser);
 
 module.exports = router;
 
